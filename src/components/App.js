ba4b2d3e6b12b4f7ebd27cca0b67310ca8f3f950
sheila <sheila18.sa@gmail.com>
2021-09-24 09:10:36 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import callToApi from './services/api';
+import ls from './services/local-storage'
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      
+    </div>
+  );
+}
+
+export default App;
