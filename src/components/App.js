8b4c432795923114d3c5d4c292de70e9ebfb01cb
sheila <sheila18.sa@gmail.com>
2021-09-29 11:23:26 +0200

    corrected issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,61 @@
-import { waitForElementToBeRemoved } from '@testing-library/dom';
+
 import { useEffect, useState } from 'react';
 import data from '../data/club.json'
 import '../styles/App.scss';
 
+
 function App() {
   const [club, setClub ]=useState(data);
   //Variables para guardar nuevos estados de los inputs
-  const[ newClub, setNewClub ]=useState({
-    name: '',
-    openOnWeekdays: false,
-    openOnWeekend: false,
+  const [name, setName] = useState(''); 
+  const [openOnWeekdays, setOpenOnWeekdays] = useState(false);
+  const [openOnWeekend, setOpenOnWeekend] = useState(false);
+  //Creamos la variable de estado  para el filter
+  const [filter, setFilter]= useState ('all');
 
-  });
+    const handleName=(ev)=>{
+      setName(ev.target.value);
+    };
+    //En el input tipo text cogeremos el value, mientras que en el de tipo chexbox recogeremos el checked, lo cual nos dira si hemos cambiado de false a true 
+    const handleOpenWeekdays=(ev)=>{
+      setOpenOnWeekdays(ev.target.checked);
+    };
+    const handleOpenOnWeekend=(ev)=>{
+      setOpenOnWeekend(ev.target.checked);
+    };
+    //En este boton, añadiremos con el push el nuevo objeto creado por el usuario
+   const handleSubmit =(ev)=>{
+     ev.preventDefault();
+     club.push({
+      name: name,
+      openOnWeekdays: openOnWeekdays,
+      openOnWeekend: openOnWeekend,
 
-   const handleNewClub=(ev)=>{
-   if(ev.currentTarget.id === 'new'){
-     setNewClub({
-       ...newClub,
-       new: ev.currentTarget.value,
      });
-   }else if(ev.currentTarget.id=== 'option1'){
-     setNewClub({
-       ...newClub,
-       openOnWeekdays: ev.currentTarget.checked,
-     });
-   }else if(ev.currentTarget.id=== 'option2'){
-    setNewClub({
-      ...newClub,
-      openOnWeekend: ev.currentTarget.checked,
-    });
-    }
+     setClub([...club])
    };
-
-  const handleNewClubButton=(ev)=>{
-    ev.preventDefault();
-    if (newClub.name !== '') {
-      setClub([...club, newClub]);
-    }
-    setNewClub({
-      name: '',
-      openOnWeekdays: false,
-      openOnWeekend: false,
-    });
-}
-
+   //Función manejadora del filtro
+   const handleFilter=(ev)=>{
+    setFilter(ev.target.value);
+   };
+  
+//Esta es la función la cual nos pinta en el index
   const renderClub=()=>{
-    return data.map((club, index)=>{
+    return club //Primero hacemos el el filtro
+    .filter((clubs)=>{
+      if (filter === 'openOnWeekDays'){
+        return clubs.openOnWeekdays===true;
+      }else if( filter=== 'openOnWeekend'){
+        return clubs.openOnWeekend ===true;
+      }
+      return true;
+    })
+    .map((club, index)=>{ //Después pintamos en el index
       return(
         <li key={index} id={index} className="list__club">
         <h3 className="name__club">{club.name}</h3>
         <p className="week__day">Abierto entre semana: {club.openOnWeekdays === true ? 'si' : 'No'}</p>
-        <p className="week__end"> abierto los fines de semana: {club.openOnWeekend === true ? 'si' : 'No' }</p>
+        <p className="week__end"> Abierto los fines de semana: {club.openOnWeekend === true ? 'si' : 'No' }</p>
         </li>
 
       )
@@ -59,35 +64,36 @@ function App() {
   return (
     <div>
       <header>
-      <h1 className="header__tittle">Mis clubs</h1>
-      <form>
-        <label htmlFor="club">Mostrar</label>
-        <select name="club" id="club">
+      <h1 className="header__title">Mis clubs</h1>
+      <form className="header_form">
+        <label htmlFor="club" className="header_form__label">Mostrar </label>
+        <select value ={filter} onChange={handleFilter} className="header_form__select">
           <option>Todos</option>
-          <option> los que abren entre semana</option>
-          <option>los que abren el fin de semana</option>
+          <option> Los que abren entre semana</option>
+          <option>Los que abren el fin de semana</option>
         </select>
       </form>
       </header>
       <main>
-      <ul className="list__club">{renderClub()}</ul>
-      <section>
+      <ul >{renderClub()}</ul>
+      <section className="main__section">
         <h2 className="main__tittle">Añadir un nuevo club</h2>
-        <form className="main__form">
+        <form className="main__form" onSubmit={handleSubmit}>
           <label className="form__label-club"> Nombre del club </label>
         <input
             className="form__input"
             type="text"
-            name="new"
-            id="new"
-            onChange={handleNewClub}
+            name="name"
+            id="name"
+            value={name}
+            onChange={handleName}
           />
           <label htmlFor="option1">¿Abre entre semana?</label>
-          <input type="checkbox" name="option1" id="option1" onChange={handleNewClub} checked={newClub.openOnWeekdays}/>
+          <input type="checkbox" name="option1" id="option1" onChange={handleOpenWeekdays} checked={openOnWeekdays}/>
           <label htmlFor="option2">¿Abre los fines de  semana?</label>
-          <input type="checkbox" name="option2" id="option2" onChange={handleNewClub} checked={newClub.openOnWeekend}/>
+          <input type="checkbox" name="option2" id="option2" onChange={handleOpenOnWeekend} checked={openOnWeekend}/>
 
-          <input type="submit" value="añadir" className="new_club" onClick={handleNewClubButton}/>
+          <input type="submit" value="añadir" className="new_club" />
 
           
         </form>
