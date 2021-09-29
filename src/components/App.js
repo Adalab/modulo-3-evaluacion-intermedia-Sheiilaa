b469f39e94ad6cafb5d4f50239b57ef943ca24fa
sheila <sheila18.sa@gmail.com>
2021-09-29 12:23:21 +0200

    new change

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import '../styles/App.scss';
 
 
 function App() {
-  const [club, setClub ]=useState(data);
+  const [clubs, setClubs ]=useState(data);
   //Variables para guardar nuevos estados de los inputs
   const [name, setName] = useState(''); 
   const [openOnWeekdays, setOpenOnWeekdays] = useState(false);
@@ -26,27 +26,34 @@ function App() {
     //En este boton, añadiremos con el push el nuevo objeto creado por el usuario
    const handleSubmit =(ev)=>{
      ev.preventDefault();
-     club.push({
+     clubs.push({
       name: name,
       openOnWeekdays: openOnWeekdays,
       openOnWeekend: openOnWeekend,
 
      });
-     setClub([...club])
+     setClubs([...clubs])
    };
    //Función manejadora del filtro
    const handleFilter=(ev)=>{
     setFilter(ev.target.value);
    };
+   const handleDelete = (ev) => {
+    ev.preventDefault();
+    const id = ev.currentTarget.id;
+    clubs.splice(id, 1);
+    setClubs([...clubs]);
+   
+  };
   
 //Esta es la función la cual nos pinta en el index
   const renderClub=()=>{
-    return club //Primero hacemos el el filtro
-    .filter((clubs)=>{
+    return clubs //Primero hacemos el el filtro
+    .filter((club)=>{
       if (filter === 'openOnWeekDays'){
-        return clubs.openOnWeekdays===true;
+        return club.openOnWeekdays===true;
       }else if( filter=== 'openOnWeekend'){
-        return clubs.openOnWeekend ===true;
+        return club.openOnWeekend ===true;
       }
       return true;
     })
@@ -56,7 +63,11 @@ function App() {
         <h3 className="name__club">{club.name}</h3>
         <p className="week__day">Abierto entre semana: {club.openOnWeekdays === true ? 'si' : 'No'}</p>
         <p className="week__end"> Abierto los fines de semana: {club.openOnWeekend === true ? 'si' : 'No' }</p>
+        <div>
+          <button className="button__delete" onClick={handleDelete}>X</button>
+        </div>
         </li>
+
 
       )
     })
@@ -92,10 +103,10 @@ function App() {
           <input type="checkbox" name="option1" id="option1" onChange={handleOpenWeekdays} checked={openOnWeekdays}/>
           <label htmlFor="option2">¿Abre los fines de  semana?</label>
           <input type="checkbox" name="option2" id="option2" onChange={handleOpenOnWeekend} checked={openOnWeekend}/>
+        
+          <input type="submit" value="Añadir nuevo club" className="new_club" />
 
-          <input type="submit" value="añadir" className="new_club" />
-
-          
+         
         </form>
 
       </section>
